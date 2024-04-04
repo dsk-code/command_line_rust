4d4cabae19ff6b7c3062a6741863b5e0a3846899
dsk-code <dsk0425lucky@iCloud.com>
2024-04-04 11:45:25 +0900

    2024-04-04

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about = "Rust版echo", long_about = None)]
+struct Args {
+    test: String,
+    #[arg(short, long, help = "input name", required = true)]
+    name: String,
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+    #[arg(short = 'p', long)]
+    check: bool,
+}
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.test);
+        println!("check = {}", args.check);
+    }
+
+    println!("{:#?}", args);
 }
